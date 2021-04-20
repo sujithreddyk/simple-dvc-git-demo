a241ced18067b846266095cd77a0d288bf967cfb
sujithreddyk <ksreddykavali@gmail.com>
2021-04-20 12:46:12 +0530

    Stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -16,7 +16,7 @@ def get_data(config_path):
     config = read_params(config_path)
     #print(config)
     data_path = config["data_source"]["s3_source"]
-    df = pd.read_csv(data_path)
+    df = pd.read_csv(data_path,sep=",",encoding="utf-8")
     #print(df.head())
     return df
 
