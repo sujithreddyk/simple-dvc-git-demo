04e2803685d25dc5d2a6dbb85989c836b8542049
sujithreddyk <ksreddykavali@gmail.com>
2021-04-21 10:46:36 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Its a Wine Quality package",
+    author="sujith",
+    packages=find_packages(),
+    license="MIT"
+)
